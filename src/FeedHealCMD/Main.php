3bb49930244db6584ee349e81939dbea83071f1d
minijaham <60860894+minijaham@users.noreply.github.com>
2020-05-03 18:16:02 -0700

    Add files via upload

---
+++ src/FeedHealCMD/Main.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace FeedHealCMD;
+
+use pocketmine\plugin\PluginBase;
+
+use FeedHealCMD\Commands\FeedCommand;
+use FeedHealCMD\Commands\HealCommand;
+
+class Main extends PluginBase
+{
+
+
+    const prefix = "§d[§bAPE§d]§f FeedAndHeal";
+
+    public static $instance;
+
+    public static function getInstance()
+    {
+
+        return self::$instance;
+
+    }
+
+    public function onEnable()
+    {
+
+        $this->getServer()->getLogger()->info(self::prefix . "Plugin Enabled");
+
+        self::$instance = $this;
+
+        $this->onCommands();
+
+    }
+
+    private function onCommands()
+    {
+
+        $this->getServer()->getCommandMap()->registerAll("command",
+            [
+
+                new FeedCommand(),
+                new HealCommand(),
+
+            ]
+
+        );
+
+    }
+
+    public function onDisable()
+    {
+
+        $this->getServer()->getLogger()->info(self::prefix . "Plugin Disabled");
+
+    }
+
+
+}
